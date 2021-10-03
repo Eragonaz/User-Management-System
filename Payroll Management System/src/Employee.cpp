3fc7efcbf65c954f49404aa8d97e2fb08f1c59f1
Muhammad Fikri <muhammadfikri6844@outlook.com>
2021-10-03 22:07:36 +0800

    Fixed merge conflict with master.

--- Payroll Management System/src/Employee.cpp
+++ Payroll Management System/src/Employee.cpp
@@ -13,27 +13,7 @@ namespace EmployeeSys {
 	Employee::Employee(const size_t userid, const std::string& username, const std::string& password) 
 		: User(userid, username, password) {
 		
-		/*
-
-		std::string surName;
-		std::string firstName;
-		std::string fullName;
-
-		size_t id;
-
-		struct Contact {
-			size_t phoneNo;
-			std::string emailAddress;
-			std::string homeAddress;
-		} contact;
-
-		struct NOKContact {
-			std::string relationship;
-			size_t phoneNo;
-			std::string emailAddress;
-			std::string homeAddress;
-		} NOKcontact;
-		*/
+		
 	}
 
 }
