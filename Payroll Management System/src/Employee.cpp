9b307b0754918cbca88a87b82a6cd8c49c48fb1c
Muhammad Fikri <muhammadfikri6844@outlook.com>
2021-10-03 21:43:44 +0800

    Adding initialization of Employee class member: PersonalDetails.

--- Payroll Management System/src/Employee.cpp
+++ Payroll Management System/src/Employee.cpp
@@ -12,6 +12,28 @@ namespace EmployeeSys {
 
 	Employee::Employee(const size_t userid, const std::string& username, const std::string& password) 
 		: User(userid, username, password) {
+		
+		/*
+
+		std::string surName;
+		std::string firstName;
+		std::string fullName;
+
+		size_t id;
+
+		struct Contact {
+			size_t phoneNo;
+			std::string emailAddress;
+			std::string homeAddress;
+		} contact;
+
+		struct NOKContact {
+			std::string relationship;
+			size_t phoneNo;
+			std::string emailAddress;
+			std::string homeAddress;
+		} NOKcontact;
+		*/
 	}
 
 }
